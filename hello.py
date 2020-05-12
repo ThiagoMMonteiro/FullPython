aa2f09798add1e4859d84ab8d71c0dc0fe856e4b
Thiago Mânica Monteiro <thiago.monteiro2608@gmail.com>
2020-05-12 12:09:58 -0300

    adicionando o primeiro arquivo ao projeto

---
+++ hello.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*- (serve para o interpretador python reconhecer os acentos)
+# Comentario de uma linha
+print ("Hello World!")
+
+
+"""
+Comentario de
+varias linhas
+"""
+
+#Operações matemáticas
+print (2 + 2) #soma
+print (2 - 2) #subtração
+print (2 / 2) #divisão
+print (2 * 2) #multiplicação
+print (2 ** 3) #exponenciação
+print (3 % 2) #módulo - resto
+
+
