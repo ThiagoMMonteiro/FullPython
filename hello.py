cffff49c6d59bf8e4edf18a778ab65261474d2a2
Thiago Mânica Monteiro <thiago.monteiro2608@gmail.com>
2020-05-12 12:22:35 -0300

    Adicionando a parte básica de variáveis

--- hello.py
+++ hello.py
@@ -16,4 +16,15 @@ print (2 * 2) #multiplicação
 print (2 ** 3) #exponenciação
 print (3 % 2) #módulo - resto
 
+#Variáveis
+variavel = 47 #int
+print(type(variavel)) 
+variavel = 47.55 #flaot
+print(type(variavel)) 
+variavel = "47.55" #str
+print(type(variavel))
+variavel = '47.55' #str
+print(type(variavel))
+variavel = True #bool
+print(type(variavel))
 
