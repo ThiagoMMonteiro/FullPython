a7d3ff87dfc96705f02905a67569db6a9328ca94
Thiago Mânica Monteiro <thiago.monteiro2608@gmail.com>
2020-06-01 13:27:33 -0300

    Adicionando Conteúdo de List Comprehension

--- hello.py
+++ hello.py
@@ -346,3 +346,13 @@ elif sinal == "*":
 	print(num1*num2)
 elif sinal == "**":
 	print(num1**num2)
+
+#List Comprehension
+x = [1, 2, 3, 4, 5]
+# y = [valor_a_adicionar laço condição]
+
+y = [i**2 for i in x]
+print(x)
+print(y)
+z = [i for i in x if i%2 == 1]
+print(z)
